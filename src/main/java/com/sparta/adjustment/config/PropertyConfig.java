95701fcbca626e1b82eabbd6092353401f6ed138
DYKIM\kdy95 <kdy950930@gmail.com>
2024-10-21 14:45:47 +0900

    프로퍼티 설정

---
+++ src/main/java/com/sparta/adjustment/config/PropertyConfig.java
@@ -0,0 +1,19 @@
+package com.sparta.adjustment.config;
+
+import org.springframework.beans.factory.config.PropertiesFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class PropertyConfig {
+    @Bean(name = "social")
+    public PropertiesFactoryBean propertiesFactoryBean(){
+        PropertiesFactoryBean propertiesFactoryBean = new PropertiesFactoryBean();
+        ClassPathResource classPathResource = new ClassPathResource("../resources/properties/*.properties");
+
+        propertiesFactoryBean.setLocation(classPathResource);
+
+        return propertiesFactoryBean;
+    }
+}
