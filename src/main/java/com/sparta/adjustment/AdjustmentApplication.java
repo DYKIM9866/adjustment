127da16c8088752fa157c36ca2cfa3122fb63e18
DYKIM\kdy95 <kdy950930@gmail.com>
2024-10-21 19:59:39 +0900

    FeignClient JpaAuditing 어노테이션 추가

--- src/main/java/com/sparta/adjustment/AdjustmentApplication.java
+++ src/main/java/com/sparta/adjustment/AdjustmentApplication.java
@@ -2,8 +2,12 @@ package com.sparta.adjustment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaAuditing
 public class AdjustmentApplication {
 
     public static void main(String[] args) {
