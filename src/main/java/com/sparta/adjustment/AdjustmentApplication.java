1660bdad5723bf71bb6ab250e7dac0ef650e6d24
DYKIM\kdy95 <kdy950930@gmail.com>
2024-11-01 21:46:33 +0900

    security 끄기

--- src/main/java/com/sparta/adjustment/AdjustmentApplication.java
+++ src/main/java/com/sparta/adjustment/AdjustmentApplication.java
@@ -2,10 +2,11 @@ package com.sparta.adjustment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableFeignClients
 @EnableJpaAuditing
 public class AdjustmentApplication {
