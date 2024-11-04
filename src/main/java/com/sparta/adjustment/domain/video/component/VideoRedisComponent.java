d34d5bc17eec74553e57a8c231267d5b4c2af813
DYKIM\kdy95 <kdy950930@gmail.com>
2024-11-04 17:48:12 +0900

    변수 롬복 제거 후 생성자 주입

--- src/main/java/com/sparta/adjustment/domain/video/component/VideoRedisComponent.java
+++ src/main/java/com/sparta/adjustment/domain/video/component/VideoRedisComponent.java
@@ -1,19 +1,21 @@
 package com.sparta.adjustment.domain.video.component;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
 import java.util.concurrent.TimeUnit;
 
 @Component
-@RequiredArgsConstructor
 public class VideoRedisComponent {
 
     private final RedisTemplate<String, Object> redisTemplate;
-    private final ValueOperations<String, Object> ops = redisTemplate.opsForValue();
+    private final ValueOperations<String, Object> ops;
+
+    public VideoRedisComponent(RedisTemplate<String, Object> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        this.ops = redisTemplate.opsForValue();
+    }
 
     public Object getCached(String key) {
         return ops.get(key);
