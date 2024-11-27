b3de13e3114a2f76689c5dc2d70fbb1a6becc457
DYKIM\kdy95 <kdy950930@gmail.com>
2024-11-28 04:09:22 +0900

    배치작업 시 jpa 쿼리 나오지 않도록 함

--- src/main/java/com/sparta/adjustment/batch/listener/MyJobExecutionListener.java
+++ src/main/java/com/sparta/adjustment/batch/listener/MyJobExecutionListener.java
@@ -1,5 +1,6 @@
 package com.sparta.adjustment.batch.listener;
 
+import com.sparta.adjustment.util.LoggerManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.annotation.AfterJob;
@@ -10,11 +11,13 @@ public class MyJobExecutionListener {
 
     @BeforeJob
     public void beforeJob(JobExecution jobExecution) {
+        LoggerManager.disableSqlLogging();
         log.info(jobExecution.getJobInstance().getJobName() + " Start!");
     }
 
     @AfterJob
     public void afterJob(JobExecution jobexecution) {
+        LoggerManager.enableSqlLogging();
         log.info(jobexecution.getJobInstance().getJobName() + " " + jobexecution.getExitStatus());
         log.info(jobexecution.getJobInstance().getJobName() + " " + jobexecution.getStatus());
     }
